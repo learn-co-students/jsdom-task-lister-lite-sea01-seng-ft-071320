8468d10d70b66e8b126175ab55e7607fbce973ed
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-08-25 16:06:33 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  listenToFormSubmit()
 });
+
+const form = document.getElementById(`create-task-form`)
+const newTask = document.getElementById(`new-task-description`)
+const list = document.getElementById(`tasks`)
+
+
+function listenToFormSubmit(){
+
+form.addEventListener('submit',function(e){
+  e.preventDefault()
+  const list = document.createElement("li")
+  const newItem = document.createTextNode(`${newTask.value}`);
+  list.appendChild(newItem);
+  document.getElementById("list").appendChild(list); 
+  
+  const button = document.createElement("button")
+  button.textContent = 'x'
+  list.appendChild(button);
+
+  list.addEventListener("click", function() {
+    list.remove()
+  })
+  
+  newTask.value = ''
+})
+};
+
+
+
+
+
+
+
+
